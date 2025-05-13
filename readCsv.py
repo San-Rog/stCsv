f64ad901a832c63488c20e4995873953a21169dd
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-13 01:09:34 -0300

    Update readCsv.py

--- readCsv.py
+++ readCsv.py
@@ -2,12 +2,5 @@ import pandas as pd
 import streamlit as st
 from datetime import date
 
-def changeValue(value):
-    try:
-        return (value.strftime("%d/%m/%Y"))
-    except:
-        return ""
-
 df = pd.read_csv('feriadosNacionais.csv')
-df["Data"] = df["Data"].apply(changeValue)
 st.dataframe(data=df, hide_index=True) 
